3e7b1d7861453ad8e4554edcb96f8d8b404fbe32
Md. Rifatul Islam <140544121+md-rifatul@users.noreply.github.com>
2025-11-12 11:42:40 +0600

    Format IRepository interface for consistency

--- Generic/IRepository.cs
+++ Generic/IRepository.cs
@@ -1,9 +1,9 @@
-public interface IRepository<T> where T: class
-{
-    IEnumerable<T> GetAll();
-    T? GetById(int id);
-    void Add(T entity);
-    void Update(T entity);
-    void Delete(T entity);
-    void Save();
-}
+ public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T? GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+    }
